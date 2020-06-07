38e39978d3ded882b2fb5f0f29405c5799514012
Chami Rachid <chamirachid1@gmail.com>
2020-06-07 08:46:28 -0700

    long size strings decoding

--- src/main/kotlin/RlpDecoder.kt
+++ src/main/kotlin/RlpDecoder.kt
@@ -5,7 +5,7 @@ object RlpDecoder {
         when(Integer.parseInt(hexEncodedInput.substring(0..1), 16)) {
             in 0x0..0x7f -> return encodedInput.toHexString().hexToAscii()
             in 0x80..0xb7 -> return encodedInput.drop(1).toByteArray().toHexString().hexToAscii()
-            in 0xb8..0xbf -> return encodedInput.slice(2..(Integer.parseInt(encodedInput[1].toString(), 16)/8)).toByteArray().toHexString().hexToAscii()
+            in 0xb8..0xbf -> return encodedInput.slice(2..Integer.parseInt(hexEncodedInput.substring(2..3), 16)).toByteArray().toHexString().hexToAscii()
         }
         return ""
     }
